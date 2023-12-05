265933ce4f57e23666f83c6168fc40653454fa67
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-12-05 14:48:06 +0200

    Functions and arrow functions

--- JavaScript Jonas Schmedtmann/Fundamentals 2/index.js
+++ JavaScript Jonas Schmedtmann/Fundamentals 2/index.js
@@ -25,3 +25,31 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(3, 2));
 const appleJuice = fruitProcessor(4, 5);
 console.log(appleJuice);
+
+// function declaration
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+
+const age1 = calcAge1(1983);
+console.log(age1);
+
+// function expression
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age2);
+
+// ==== arrow functions ====
+const calcAge3 = (birthYear) => 2023 - birthYear;
+console.log(calcAge3(1991));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years!`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(2015, 'Andrei'));
